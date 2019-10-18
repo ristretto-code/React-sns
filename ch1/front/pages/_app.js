0e3ebe01953ac1683d7a41b4aa129183afef18a0
IANCHOI <endnd28@gmail.com>
2019-10-18 20:28:47 +0900

    redux_devtools setting

--- ch1/front/pages/_app.js
+++ ch1/front/pages/_app.js
@@ -5,7 +5,7 @@ import reducer from "../reducers";
 import propTypes from "prop-types";
 import withRedux from "next-redux-wrapper";
 import { Provider } from "react-redux";
-import { createStore, compose, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 
 const ReactSns = ({ Component, store }) => {
   return (
@@ -25,19 +25,18 @@ const ReactSns = ({ Component, store }) => {
 };
 
 ReactSns.propTypes = {
-  Component: propTypes.elementType, // 모든 자료형을 포함하는 node
+  Component: propTypes.elementType, // 모든 자료형을 포함하는 것은 node
   store: propTypes.object
 };
 
 export default withRedux((initialState, options) => {
   const middlewares = [];
-  const enhancer = compose(
-    applyMiddleware(...middlewares),
-    typeof window !== "undefined" &&
-      window.__REDUX_DEVTOOLS_EXTENTION__ !== "undefined"
-      ? window.__REDUX_DEVTOOLS_EXTENTION__()
-      : f => f
-  );
+  const composeEnhancers =
+    typeof window === "object" && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+      ? // redux_devtools가 일반사용자에게 노출되지 않게해야함. next에 내장된 ootions.isServer도 쓸수있는듯함
+        window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
+      : compose;
+  const enhancer = composeEnhancers(applyMiddleware(...middlewares));
   const store = createStore(reducer, initialState, enhancer);
-  return store; // 위로 4줄은 대부분 프로젝트에서 바뀔일이없는 코드. 배열안에 미들웨어들만 넣어주면 된다.
+  return store; // 위로 6줄은 대부분 프로젝트에서 바뀔일이없는 코드. 배열안에 미들웨어들만 넣어주면 된다.
 })(ReactSns);

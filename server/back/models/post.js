e3899e93327d0d75698c67441f380bd2f795922d
IANCHOI <endnd28@gmail.com>
2019-10-24 21:38:11 +0900

    sequelize table create

---
+++ server/back/models/post.js
@@ -0,0 +1,22 @@
+module.exports = (sequelize, DataTypes) => {
+  const Post = sequelize.define(
+    "Post",
+    {
+      content: {
+        type: DataTypes.TEXT, // 매우긴글
+        allowNull: false
+      }
+    },
+    {
+      charset: "utf8mb4",
+      collate: "utf8mb4_general_ci"
+    }
+  );
+
+  Post.associate = db => {
+    db.Post.belongsTo(db.User);
+    db.Post.hasMany(db.Comment);
+    db.Post.hasMany(db.Image);
+  };
+  return Post;
+};

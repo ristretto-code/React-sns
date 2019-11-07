aa5c9d5af5a4faa5b3eb796fc9965018564dd2aa
IANCHOI <endnd28@gmail.com>
2019-11-07 23:59:12 +0900

    Post api

--- server/back/routes/post.js
+++ server/back/routes/post.js
@@ -6,18 +6,20 @@ router.post("/", async (req, res, next) => {
   try {
     const hashtags = req.body.content.match(/#[^\s]+/g);
     const newPost = await db.Post.create({
-      content: req.body.content
+      content: req.body.content,
+      Userid: req.user.id
     });
     if (hashtags) {
       const result = await Promise.all(
         hashtags.map(tag =>
           db.Hashtag.findOrCreate({
-            where: { name: tag.slice(1).toLowerCase() }
+            // hastags 테이블에서 찾아봐서 없으면 create
+            where: { name: tag.slice(1).toLowerCase() } // 앞에 # 뗀다음 전부 소문자로 저장
           })
         )
       );
       console.log(result);
-      await newPost.addHashtags(result.map(r => r[0]));
+      await newPost.addHashtags(result.map(r => r[0])); //시퀄라이즈가 만들어준 addHashtags
     }
     res.json(newPost);
   } catch (e) {

0eabdb903e11ab3a6e17b0ce1e44235a33bd468b
IANCHOI <endnd28@gmail.com>
2019-11-15 18:47:24 +0900

    add front custom server for dynamic routing

---
+++ front/server.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const next = require("next");
+const morgan = require("morgan");
+const cookieParser = require("cookie-parser");
+const expressSession = require("express-session");
+const dotenv = require("dotenv");
+
+// 커스텀서버를 위해 node환경과 똑같이 불러와준다. env도 마찬가지
+
+const dev = process.env.NODE_ENV !== "production";
+const prod = process.env.NODE_ENV === "production";
+const app = next({ dev }); // express로 next 돌리기,  option에 dev모드넣어줬다
+const handle = app.getRequestHandler(); // 요청처리기
+
+dotenv.config();
+
+app.prepare().then(() => {
+  // 준비되면
+  const server = express();
+
+  server.use(morgan("dev"));
+  server.use(express.json());
+  server.use(express.urlencoded({ extended: true }));
+  server.use(cookieParser(process.env.COOKIE_SECRET));
+  server.use(
+    expressSession({
+      resave: false,
+      saveUninitialized: false,
+      secret: process.env.COOKIE_SECRET,
+      cookie: {
+        httpOnly: true,
+        secure: false
+      }
+    })
+  );
+
+  server.get("*", (req, res) => {
+    return handle(req, res);
+  });
+  server.listen(3000, () => {
+    console.log("next+express running on 3000");
+  });
+});

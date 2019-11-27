c6ffa75520d24cf877960bea8f37f9bd82b03e79
IANCHOI <endnd28@gmail.com>
2019-11-27 18:08:51 +0900

    backend make Post_Comment api

--- back/routes/post.js
+++ back/routes/post.js
@@ -45,5 +45,63 @@ router.post("/", async (req, res, next) => {
   }
 }); // 게시글 작성 api/post
 router.post("/images", (req, res) => {}); // 이미지올리기
+router.get("/:id/comments", async (req, res, next) => {
+  try {
+    const post = await db.Post.findOne({ where: { id: req.params.id } });
+    if (!post) {
+      return res.status(404).send("포스트가 존재하지 않습니다");
+    }
+    const comments = await db.Comment.findall({
+      where: {
+        PostId: req.params.id
+      },
+      order: [["created", "ASC"]],
+      include: [
+        {
+          model: db.User,
+          attributes: ["id", "nickname"]
+        }
+      ]
+    });
+    return res.json(comments);
+  } catch (e) {
+    console.error(e);
+    return next(e);
+  }
+}); // 이미지올리기
+router.post("/:id/comments", async (req, res, next) => {
+  // POST /api/post/1000/comment
+  try {
+    if (!req.user) {
+      return res.status(401).send("로그인이 필요합니다");
+    }
+    const post = await db.Post.findOne({ where: { id: req.params.id } });
+    if (!post) {
+      return res.status(404).send("포스트가 존재하지 않습니다");
+    }
+    const newComment = await db.Comment.create({
+      PostId: post.id,
+      UserId: req.user.id,
+      content: req.body.content
+    });
+    await post.addComment(newComment.id); //시퀄라이져에서 add랑 new넣어줌
+    const comment = await db.Comment.findOne({
+      // include사용하기 위해 새로 find
+      where: {
+        id: newComment.id
+      },
+      include: [
+        {
+          model: db.User,
+          attributes: ["id", "nickname"]
+        }
+      ]
+    });
+    return res.json(comment);
+  } catch (e) {
+    console.error(e);
+    return next(e);
+  }
+}); // 이미지올리기
 
 module.exports = router;

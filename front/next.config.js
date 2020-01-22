179f540dd0fcbde4dee4aa0a1bf5b366e0405dcb
IANCHOI <endnd28@gmail.com>
2020-01-22 14:52:20 +0900

    Update next.config.js

--- front/next.config.js
+++ front/next.config.js
@@ -10,34 +10,32 @@ module.exports = {
     // console.log("config", config); // default webpack setting - resolve
     // console.log("rules", config.module.rules[0]);
 
-    config.module.rules.push(
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/,
-        use: [
-          {
-            loader: "url-loader",
-            options: {
-              limit: 10000,
-              fallback: "file-loader",
-              name: "fonts/[name].[ext]"
-            }
+    config.module.rules.push({
+      test: /\.(woff|woff2|eot|ttf|otf)$/,
+      use: [
+        {
+          loader: "url-loader",
+          options: {
+            limit: 10000,
+            fallback: "file-loader",
+            name: "fonts/[name].[ext]"
           }
-        ]
-      },
-      {
-        test: /\.(jpg|jpeg|gif|png|svg|ico)?$/,
-        use: [
-          {
-            loader: "url-loader",
-            options: {
-              limit: 10000,
-              fallback: "file-loader",
-              name: "images/[name].[ext]"
-            }
+        }
+      ]
+    });
+    config.module.rules.push({
+      test: /\.(jpg|jpeg|gif|png|svg|ico)?$/,
+      use: [
+        {
+          loader: "url-loader",
+          options: {
+            limit: 10000,
+            fallback: "file-loader",
+            name: "images/[name].[ext]"
           }
-        ]
-      }
-    );
+        }
+      ]
+    });
     const prod = process.env.NODE_ENV === "production";
     const plugins = [
       ...config.plugins,

6b50811fb9e0a857802a7f1c00c28057e3a3f896
IANCHOI <endnd28@gmail.com>
2020-01-23 12:08:59 +0900

    Update : Hashtag page blocked dispatch repeat

--- front/pages/hashtag.js
+++ front/pages/hashtag.js
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect } from "react";
+import React, { useCallback, useEffect, useRef } from "react";
 import PropTypes from "prop-types";
 import { useSelector, useDispatch } from "react-redux";
 import { LOAD_HASHTAG_POSTS_REQUEST } from "../reducers/post";
@@ -7,18 +7,23 @@ import PostCard from "../components/PostCard";
 const Hashtag = ({ tag }) => {
   const dispatch = useDispatch();
   const { mainPosts, hasMorePost } = useSelector(state => state.post);
+  const countHashRef = useRef([]);
 
   const onScroll = useCallback(() => {
     if (
       window.scrollY + document.documentElement.clientHeight >
-      document.documentElement.scrollHeight - 300
+      document.documentElement.scrollHeight - 400
     ) {
       if (hasMorePost) {
-        dispatch({
-          type: LOAD_HASHTAG_POSTS_REQUEST,
-          lastId: mainPosts[mainPosts.length - 1].id,
-          data: tag
-        });
+        const lastId = mainPosts[mainPosts.length - 1].id;
+        if (!countHashRef.current.includes(lastId)) {
+          dispatch({
+            type: LOAD_HASHTAG_POSTS_REQUEST,
+            data: tag,
+            lastId: lastId
+          });
+          countHashRef.current.push(lastId);
+        }
       }
     }
   }, [mainPosts.length, hasMorePost]);
